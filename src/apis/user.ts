24495bbb5274a81e7d9897c7826dd9174aee092e
than-nl <than-nl@mcsolutions.vn>
2024-04-22 23:44:57 +0700

    init source

---
+++ src/apis/user.ts
@@ -0,0 +1,9 @@
+const createUser = async () => {
+  //   try {
+  //     await axiosInstance.post();
+  //   } catch (error) {
+  //     return Promise.reject(error);
+  //   }
+};
+
+export { createUser };
